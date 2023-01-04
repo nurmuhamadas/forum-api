bf251b61caf17385e1a624b1ab39f4e0e64b48eb
nurmuhamadas <nurmuhamad.a.13@gmail.com>
2023-01-04 12:38:10 +0700

    fix: initial commit

---
+++ migrations/1672775004183_create-table-thread.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      default: false,
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
---
+++ migrations/1672810368844_create-table-comments.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'threads(id)',
+      onDelete: 'CASCADE',
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'CASCADE',
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      default: false,
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    updated_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
